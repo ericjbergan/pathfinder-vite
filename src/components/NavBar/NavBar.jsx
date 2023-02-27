17d74bd12563d1ea42727ec7b11c743c7f252ff0
ericjbergan <ericjbergan@gmail.com>
2023-02-27 13:05:43 -0800

    css working for header

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,20 +1,24 @@
 import { React, createContext } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import pathfinderLogo from '../../assets/pathfinderLogo.jpg'
+import App from '../../App'
 import NewChar from '../../pages/NewCharacter/NewCharacter'
 import SavedChar from '../../pages/SavedCharacters/SavedCharacters'
+import './NavBar.css'
 
 
 export default function NavBar() {
     return (
-        <div>
+        <div className='main'>
             <img src={pathfinderLogo} className='path-logo' alt='Pathfinder Logo' />
             <nav className='navbar'>
+                <a href='home'>Home</a>
                 <a href="/new-character">Create Character</a>
                 <a href="/saved-characters">Load Character</a>
             </nav>
             <Router>
                 <Switch>
+                    <Route exact path='home' component={ App } />
                     <Route exact path='/new-character' component={ NewChar } />
                     <Route exact path='/saved-characters' component={ SavedChar } />
                 </Switch>
